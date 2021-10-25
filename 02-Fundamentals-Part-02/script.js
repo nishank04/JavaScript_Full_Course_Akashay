eb3181666117a413914e87e91007623929bc6a80
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2021-10-25 17:40:07 +0530

    Array methods

--- 02-Fundamentals-Part-02/script.js
+++ 02-Fundamentals-Part-02/script.js
@@ -23,6 +23,7 @@
 //     return 2037 - birthYear;
 // }
 
+
 // const age1 = calcAge1(1989);
 
 // ///Function Expression
@@ -96,18 +97,129 @@ calcAverage = (score1, score2, score3) => {
     
 // var averageDolphins = calcAverage(44, 23, 71);
 // var averageKoalas = calcAverage(65, 54, 49);
-var averageDolphins = calcAverage(85, 54, 41);
-var averageKoalas = calcAverage(23, 34, 27);
-console.log(averageDolphins, averageKoalas);
+// var averageDolphins = calcAverage(85, 54, 41);
+// var averageKoalas = calcAverage(23, 34, 27);
+// console.log(averageDolphins, averageKoalas);
 
-function checkWinner(averageDolphins, averageKoalas) {
-if (averageDolphins >= 2 * averageKoalas) {
-    console.log(`Dolphins win ${averageDolphins} vs. ${averageKoalas}`);
-} else if (averageKoalas >= 2* averageDolphins) {
-    console.log(`Koalas win ${averageKoalas} vs. ${averageDolphins}`);
-} else {
-    console.log('No team won');
-}
-}
-checkWinner(averageDolphins, averageKoalas);
+// function checkWinner(averageDolphins, averageKoalas) {
+// if (averageDolphins >= 2 * averageKoalas) {
+//     console.log(`Dolphins win ${averageDolphins} vs. ${averageKoalas}`);
+// } else if (averageKoalas >= 2* averageDolphins) {
+//     console.log(`Koalas win ${averageKoalas} vs. ${averageDolphins}`);
+// } else {
+//     console.log('No team won');
+// }
+// }
+// checkWinner(averageDolphins, averageKoalas);
     
+
+///Introduction to Arrays
+// const friend1 = 'Tiger';
+// const friend2 = 'Micjael';
+// const friend3 = 'Teddy';
+
+// const friends = ['Tiger', 'Michael', 'Teddy'];
+// console.log(friends);
+
+// const y = new Array(1991, 1989, 1984, 1995);
+// console.log(y);
+
+// console.log(friends[2]);
+// console.log(friends[0]);
+// console.log(friends[1]);
+
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
+
+// friends[2] = 'Jay';
+// console.log(friends);
+
+// const firstName = 'Akshay';
+// const akshay = [firstName, 'Kumar', 2037 - 1991, 'actor', friends]
+// console.log(akshay);
+// console.log(akshay.length);
+
+// //exercise
+// const calcAge = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const years = [1990, 1967, 2002, 2010, 2021];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 4]);
+
+// console.log(age1, age2, age3);
+
+// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 4]) ];
+// console.log(ages);
+
+////Basic Array operations
+
+// const friends = ['Michael', 'Steven', 'Peter'];
+
+// ///Add Elements
+// const newLength = friends.push('Jay');
+// console.log(newLength);
+// console.log(friends);
+
+// friends.unshift('John');
+// console.log(friends);
+
+// ///Remove Elements
+// friends.pop();
+// console.log(friends);
+
+// friends.shift();
+// console.log(friends);
+
+// console.log(friends.indexOf('Steven'));
+// console.log(friends.indexOf('Bob'));
+
+// friends.push(23);
+// console.log(friends);
+// console.log(friends.includes('Steven'));
+// console.log(friends.includes('Bob'));
+// console.log(friends.includes(23));
+
+// if (friends.includes('Nishank')) {
+//    console.log(`You have a friend called Steven`);
+// }
+
+
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44*/
+
+// const calcTip = function (bill) {
+//     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+//   }
+//   // const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  
+//   const bills = [125, 555, 44];
+//   const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+//   const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+  
+//   console.log(bills, tips, totals);
+
+  const bill = 100;
+
+function calcTip(bill) {
+  if (bill > 50 && bill < 300){
+    return  bill*.15;
+  } else {
+    return bill*.20;
+  }
+}
+const tips = calcTip(bill);
+const totalBill = bill + tips;
+console.log(totalBill);
