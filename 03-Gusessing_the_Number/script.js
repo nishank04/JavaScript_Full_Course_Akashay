4c987e4bdc0ff52ac0e84f70daf0c40e3893347f
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2021-11-10 17:34:50 +0530

    Did some css and js logic to win and loose the game

---
+++ 03-Gusessing_the_Number/script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+
+let secretNumber = Math.trunc(Math.random() *20) + 1;
+
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message
+}
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+
+    console.log(guess, typeof guess)
+
+    ///when thers no input
+    if(!guess) {
+        //document.querySelector('.message').textContent = `No number`;
+    displayMessage('No number');
+
+    //when player wins
+    } else if (guess === secretNumber){
+        displayMessage(`Correct Number`)
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    //when guess is wrong
+    } else if(guess !== secretNumber){
+        if (score > 1) {
+            // document.querySelector('.message').textContent =
+            // guess > secretNumber ? 'Too High' : 'Too Low';
+            displayMessage(guess > secretNumber ? `Too High` : `Too Low`);
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            displayMessage('You lost the game');
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+
+
+
+})
